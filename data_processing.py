c32b1406558f5382ac89d08c7ccb5e0811dea5f4
Thanawat <taroofsatoeimo@gmail.com>
2023-11-15 09:46:17 +0700

    revert the lastest commit

--- data_processing.py
+++ data_processing.py
@@ -71,22 +71,10 @@ class Table:
                 filtered_table.table.append(item1)
         return filtered_table
 
-    def __is_float(self, element):
-        if element is None:
-            return False
-        try:
-            float(element)
-            return True
-        except ValueError:
-            return False
-
     def aggregate(self, function, aggregation_key):
         temps = []
         for item1 in self.table:
-            if self.__is_float(item1[aggregation_key]):
-                temps.append(float(item1[aggregation_key]))
-            else:
-                temps.append(item1[aggregation_key])
+            temps.append(float(item1[aggregation_key]))
         return function(temps)
     
     def select(self, attributes_list):
